5585773627318c0cd9d674677c25763806d4af19
clinicpen <clinicpen1@gmail.com>
2021-02-05 09:01:22 +0200

    make it better

--- pages/index.js
+++ pages/index.js
@@ -1,63 +1,74 @@
 import Head from 'next/head'
+import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>ClinicBook.org</title>
+        <link rel='icon' href='/favicon.ico' />
       </Head>
+      <div className='byline' style={{ marginRight: '0' }}>
+        <button
+          onClick={() =>
+            window.location.replace('http://clinicbook.org/register')
+          }
+          className={styles.button}
+        >
+          <span>Create free account </span>
+        </button>
+        <span>
+          Already have account?{' '}
+          <a style={{ color: 'tomato' }} href='http://clinicbook.org'>
+            login here
+          </a>
+        </span>
+      </div>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href='http://clinicbook.org'>clinicbook.org</a>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          A smart online software for clinic adminstration..
         </p>
 
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+          <a href='http://clinicbook.org' className={styles.card}>
+            <h3>Start a happy clinic day</h3>
+            <Image
+              src='/2323.jpg'
+              alt='Picture of the author'
+              width={500}
+              height={500}
+            />
           </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+          <a href='http://clinicbook.org' className={styles.card}>
+            <h3>Stay successful</h3>
+            <Image
+              src='/face.jpeg'
+              alt='Picture of the author'
+              width={500}
+              height={500}
+            />
           </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
+          <a href='http://clinicbook.org' className={styles.card}>
+            <h3>Start for free</h3>
+            <Image
+              src='/free.jpg'
+              alt='Picture of the author'
+              width={500}
+              height={500}
+            />
           </a>
         </div>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+        <a>
+          <p> Powered by Ayman Hamoda Software</p>
         </a>
       </footer>
     </div>
